70a7049d582ef33b92900d5b28e8f104756542ed
Blaine Richardson <blainerrichardson@gmail.com>
2021-05-16 20:30:07 -0700

    feat: section-4: finishing section-4 lecture 'No Logging System' and all of stection-4

--- app/app.go
+++ app/app.go
@@ -1,15 +1,24 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"bookstore_users-api/logger"
+
+	"github.com/gin-gonic/gin"
+)
 
 var (
 	router = gin.Default()
 )
 
 func StartApp() {
+	logger.Info("{}app::#StartApp::starting execution")
 	initRoutes()
 
+	logger.Info("{}app::#StartApp::starting server")
 	if err := router.Run(":8080"); err != nil {
 		panic(err)
 	}
+	logger.Info("{}app::#StartApp::started server")
+
+	logger.Info("{}app::#StartApp::finishing execution")
 }
--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.5 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
 	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
 	golang.org/x/sys v0.0.0-20210507161434-a76c4d0a0096 // indirect
 	golang.org/x/text v0.3.6 // indirect
