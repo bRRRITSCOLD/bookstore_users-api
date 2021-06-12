585618cdd4f24334186757714f6a0d4ace37aa31
Blaine Richardson <blainerrichardson@gmail.com>
2021-06-12 18:11:06 -0400

    chore: update pkgs

--- controllers/users/users.go
+++ controllers/users/users.go
@@ -27,13 +27,13 @@ func CreateUser(c *gin.Context) {
 
 	if shouldBindJSONErr := c.ShouldBindJSON(&user); shouldBindJSONErr != nil {
 		apiError := errors_utils.NewBadRequestAPIError("invalid json body", shouldBindJSONErr)
-		c.JSON(apiError.Status(), apiError)
+		c.JSON(apiError.Status(), apiError.JSON())
 		return
 	}
 
 	createUserResult, createUserErr := users_service.UsersService.CreateUser(user)
 	if createUserErr != nil {
-		c.JSON(createUserErr.Status(), createUserErr)
+		c.JSON(createUserErr.Status(), createUserErr.JSON())
 		return
 	}
 
@@ -43,19 +43,19 @@ func CreateUser(c *gin.Context) {
 func GetUserByUserID(c *gin.Context) {
 	authenticateUserRequestErr := oauth.AuthenticateRequest(c.Request)
 	if authenticateUserRequestErr != nil {
-		c.JSON(authenticateUserRequestErr.Status(), authenticateUserRequestErr)
+		c.JSON(authenticateUserRequestErr.Status(), authenticateUserRequestErr.JSON())
 		return
 	}
 
 	userId, userIdErr := parseUserIDFromRequestPath(c.Param("userId"))
 	if userIdErr != nil {
-		c.JSON(userIdErr.Status(), userIdErr)
+		c.JSON(userIdErr.Status(), userIdErr.JSON())
 		return
 	}
 
 	getUserResult, getUserErr := users_service.UsersService.GetUserByUserID(userId)
 	if getUserErr != nil {
-		c.JSON(getUserErr.Status(), getUserErr)
+		c.JSON(getUserErr.Status(), getUserErr.JSON())
 		return
 	}
 
@@ -72,13 +72,13 @@ func LoginUser(c *gin.Context) {
 
 	if shouldBindJSONErr := c.ShouldBindJSON(&userLoginRequest); shouldBindJSONErr != nil {
 		apiError := errors_utils.NewBadRequestAPIError("invalid json body", shouldBindJSONErr)
-		c.JSON(apiError.Status(), apiError)
+		c.JSON(apiError.Status(), apiError.JSON())
 		return
 	}
 
 	user, loginUserErr := users_service.UsersService.LoginUser(userLoginRequest)
 	if loginUserErr != nil {
-		c.JSON(loginUserErr.Status(), loginUserErr)
+		c.JSON(loginUserErr.Status(), loginUserErr.JSON())
 		return
 	}
 
@@ -88,7 +88,7 @@ func LoginUser(c *gin.Context) {
 func PutUserByUserID(c *gin.Context) {
 	userId, userIdErr := parseUserIDFromRequestPath(c.Param("userId"))
 	if userIdErr != nil {
-		c.JSON(userIdErr.Status(), userIdErr)
+		c.JSON(userIdErr.Status(), userIdErr.JSON())
 		return
 	}
 
@@ -96,7 +96,7 @@ func PutUserByUserID(c *gin.Context) {
 
 	if shouldBindJSONErr := c.ShouldBindJSON(&user); shouldBindJSONErr != nil {
 		apiError := errors_utils.NewBadRequestAPIError("invalid json body", shouldBindJSONErr)
-		c.JSON(apiError.Status(), apiError)
+		c.JSON(apiError.Status(), apiError.JSON())
 		return
 	}
 
@@ -104,7 +104,7 @@ func PutUserByUserID(c *gin.Context) {
 
 	updateUserResult, updateUserErr := users_service.UsersService.PutUserByUserID(user)
 	if updateUserErr != nil {
-		c.JSON(updateUserErr.Status(), updateUserErr)
+		c.JSON(updateUserErr.Status(), updateUserErr.JSON())
 		return
 	}
 
@@ -114,7 +114,7 @@ func PutUserByUserID(c *gin.Context) {
 func PatchUserByUserID(c *gin.Context) {
 	userId, userIdErr := parseUserIDFromRequestPath(c.Param("userId"))
 	if userIdErr != nil {
-		c.JSON(userIdErr.Status(), userIdErr)
+		c.JSON(userIdErr.Status(), userIdErr.JSON())
 		return
 	}
 
@@ -122,7 +122,7 @@ func PatchUserByUserID(c *gin.Context) {
 
 	if shouldBindJSONErr := c.ShouldBindJSON(&user); shouldBindJSONErr != nil {
 		apiError := errors_utils.NewBadRequestAPIError("invalid json body", shouldBindJSONErr)
-		c.JSON(apiError.Status(), apiError)
+		c.JSON(apiError.Status(), apiError.JSON())
 		return
 	}
 
@@ -130,7 +130,7 @@ func PatchUserByUserID(c *gin.Context) {
 
 	updateUserResult, updateUserErr := users_service.UsersService.PatchUserByUserID(user)
 	if updateUserErr != nil {
-		c.JSON(updateUserErr.Status(), updateUserErr)
+		c.JSON(updateUserErr.Status(), updateUserErr.JSON())
 		return
 	}
 
@@ -140,13 +140,13 @@ func PatchUserByUserID(c *gin.Context) {
 func DeleteUserByUserID(c *gin.Context) {
 	userId, userIdErr := parseUserIDFromRequestPath(c.Param("userId"))
 	if userIdErr != nil {
-		c.JSON(userIdErr.Status(), userIdErr)
+		c.JSON(userIdErr.Status(), userIdErr.JSON())
 		return
 	}
 
 	deleteUserResult, deleteUserErr := users_service.UsersService.DeleteUserByUserID(userId)
 	if deleteUserErr != nil {
-		c.JSON(deleteUserErr.Status(), deleteUserErr)
+		c.JSON(deleteUserErr.Status(), deleteUserErr.JSON())
 		return
 	}
 
@@ -159,7 +159,7 @@ func SearchUsers(c *gin.Context) {
 
 	users, err := users_service.UsersService.SearchUsers(status)
 	if err != nil {
-		c.JSON(err.Status(), err)
+		c.JSON(err.Status(), err.JSON())
 		return
 	}
 
--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module bookstore_users-api
 go 1.16
 
 require (
-	github.com/bRRRITSCOLD/bookstore_oauth-go v0.0.0-20210611204710-9f450fc2055e // indirect
-	github.com/bRRRITSCOLD/bookstore_utils-go v0.0.0-20210611191653-3ba0942fcd68 // indirect
+	github.com/bRRRITSCOLD/bookstore_oauth-go v0.0.0-20210612180953-93a62a864e52 // indirect
+	github.com/bRRRITSCOLD/bookstore_utils-go v0.0.0-20210612180122-3727f9c5a952 // indirect
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/jmoiron/sqlx v1.3.4
